ce1e3a6461ca75001058c1fcb1796305ea2f049b
Banji Adewumi <banji@Banjis-Mac.local>
2018-12-12 02:41:11 -0800

    make custom calculate button

--- Window Shopper/Controllers/MainVC.swift
+++ Window Shopper/Controllers/MainVC.swift
@@ -9,15 +9,27 @@
 import UIKit
 
 class MainVC: UIViewController {
-
+    
+    @IBOutlet weak var wageTxt: CurrencyTxtField!
+    
+    @IBOutlet weak var priceTxt: CurrencyTxtField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let calcBtn = UIButton(frame: CGRect(x: 0, y: 0, width: view.frame.size.width, height: 60.0))
+        calcBtn.backgroundColor = #colorLiteral(red: 1, green: 0.7581974334, blue: 0.05730451032, alpha: 1)
+        calcBtn.setTitle("Calculate", for: .normal) // Always use "setTitle" on a button instead of just "title".
+        calcBtn.setTitleColor(#colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0), for: .normal)
+        calcBtn.addTarget(self, action: #selector(MainVC.calculate), for: .touchUpInside)
+        
+        wageTxt.inputAccessoryView = calcBtn
+        priceTxt.inputAccessoryView = calcBtn
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @objc func calculate() {
+        
+        print("We have gotten here")
     }
 
 
